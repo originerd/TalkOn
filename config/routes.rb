d3ecf1989f2116e2bcef078c9b0b437e76669f44
Jitae Kim <originerd@gmail.com>
2015-03-23 00:27:43 +0900

    Added api sessions to routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: [:index, :show, :create, :update, :destroy]
+      resources :sessions, only [:create, :destroy]
     end                                
   end
 end
