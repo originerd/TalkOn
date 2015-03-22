b458736ac016bd6730b1a0d7e4b94560237dee6c
Jitae Kim <originerd@gmail.com>
2015-03-22 23:16:10 +0900

    For test, protect_form_forgery to null_session in application controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,21 +3,21 @@ class Api::V1::UsersController < ApplicationController
   
   def index
     @users = User.select(:id, :email, :name).all
-    render :json => @users
+    render json: @users
   end
 
   def show
     @user = User.select(:id, :email, :name).find(params[:id]) 
-    render :json => @user
+    render json: @user
   end
 
   def create
     @user = User.new(user_params)
 
     if @user.save
-      render :json => { result: "success", user: @user}
+      render json: { result: "success", user: @user}
     else
-      render :json => { result: "fail", errors: @user.errors}
+      render json: { result: "fail", errors: @user.errors}
     end
   end
 
@@ -25,16 +25,16 @@ class Api::V1::UsersController < ApplicationController
     @user = User.find(params[:id])
 
     if @user.update_attributes(user_params)
-      render :json => { result: "success", user: @user }
+      render json: { result: "success", user: @user }
     else
-      render :json => { result: "fail", errors: @user.errors }
+      render json: { result: "fail", errors: @user.errors }
     end
   end
 
   def destroy
     User.find(params[:id]).destroy
 
-    render :json => { result: "success" }
+    render json: { result: "success" }
   end
 
   private
